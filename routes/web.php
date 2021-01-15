966a581cc5c402b36a5a1ebd6bf2a161a3811240
alexandrumuntenas <59341776+alexandrumuntenas@users.noreply.github.com>
2021-01-15 23:57:56 +0100

    ⚡️ Actualizar los controladores en web.php

--- routes/web.php
+++ routes/web.php
@@ -22,22 +22,24 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 //Rutas E-Learning
-Route::get('/elearning/inicio', [App\Http\Controllers\HomeController::class, 'index'])->name('ol_home');
+Route::get('/elearning/inicio', [ App\Http\Controllers\HomeController::class, 'index'])->name('ol_home');
+Route::get('/elearning/clase/', [App\Http\Controllers\ElearningController::class, 'classroom'])->name('ol_home');
 
 //Rutas comunicaciones
-Route::get('/comunicaciones/', [App\Http\Controllers\HomeController::class, 'index'])->name('comunicaciones_home');
+Route::get('/comunicaciones/', [App\Http\Controllers\ComunicacionesController::class, 'index'])->name('comunicaciones_home');
 
 //Rutas Agenda
-Route::get('/agenda/', [App\Http\Controllers\HomeController::class, 'index'])->name('agenda_home');
+Route::get('/agenda/', [App\Http\Controllers\AgendapersonalController::class, 'index'])->name('agenda_home');
+
 //Rutas biblioteca
-Route::get('/biblioteca/', [App\Http\Controllers\HomeController::class, 'index'])->name('biblio_home');
-Route::get('/biblioteca/catalogo', [App\Http\Controllers\HomeController::class, 'index'])->name('biblio_catalogo');
-Route::get('/biblioteca/misprestamos', [App\Http\Controllers\HomeController::class, 'index'])->name('biblio_misprestamos');
-Route::get('/biblioteca/misestadisticas', [App\Http\Controllers\HomeController::class, 'index'])->name('biblio_misestadisticas');
-Route::get('/biblioteca/misvaloraciones', [App\Http\Controllers\HomeController::class, 'index'])->name('biblio_misvaloraciones');
+Route::redirect('biblioteca/', 'biblioteca/catalogo/')->name('biblio_home');
+Route::get('/biblioteca/catalogo', [App\Http\Controllers\BibliotecaController::class, 'index'])->name('biblio_catalogo');
+Route::get('/biblioteca/misprestamos', [App\Http\Controllers\BibliotecaController::class, 'misprestamos'])->name('biblio_misprestamos');
+Route::get('/biblioteca/misestadisticas', [App\Http\Controllers\BibliotecaController::class, 'index'])->name('biblio_misestadisticas');
+Route::get('/biblioteca/misvaloraciones', [App\Http\Controllers\BibliotecaController::class, 'index'])->name('biblio_misvaloraciones');
 
 //Rutas comunidades
-Route::get('/comunidad/missuscripciones', [App\Http\Controllers\HomeController::class, 'index'])->name('cm_missuscripciones');
-Route::get('/comunidad/misprestamos', [App\Http\Controllers\HomeController::class, 'index'])->name('biblio_misprestamos');
-Route::get('/comunidad/misestadisticas', [App\Http\Controllers\HomeController::class, 'index'])->name('biblio_misestadisticas');
-Route::get('/comunidad/misvaloraciones', [App\Http\Controllers\HomeController::class, 'index'])->name('biblio_misvaloraciones');
+Route::get('/comunidad/missuscripciones', [App\Http\Controllers\ComunidadController::class, 'missuscripciones'])->name('cm_missuscripciones');
+Route::get('/comunidad/misprestamos', [App\Http\Controllers\Modulos\ComunidadController::class, 'index'])->name('biblio_misprestamos');
+Route::get('/comunidad/misestadisticas', [App\Http\Controllers\Modulos\ComunidadController::class, 'index'])->name('biblio_misestadisticas');
+Route::get('/comunidad/misvaloraciones', [App\Http\Controllers\Modulos\ComunidadController::class, 'index'])->name('biblio_misvaloraciones');
