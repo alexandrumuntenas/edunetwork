3458b2362fdbeb0ee28c790a3035f9181fe5ba6e
59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2021-02-02 08:30:06 +0100

    ➕ Añadir summernote de Laravel AdminLTE de Jeroennoten

---
+++ resources/views/modulos/notificaciones/index.blade.php
@@ -0,0 +1,35 @@
+@extends('adminlte::page')
+
+@section('title', 'Notificaciones < Edunetwork') @section('content_header') <h1>Notificaciones</h1>
+@stop
+
+@section('content')
+
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <!-- /.card-header -->
+                <div class="card-body">
+
+                </div>
+                <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+        </div>
+        <!-- /.col -->
+    </div>
+
+
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+@stop
+
+@section('footer')
+    Edunetwork v1.0
+</> by duoestudios
+@endsection
