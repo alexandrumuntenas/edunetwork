62db755e129da189f95094185dcc4dc069785bbb
59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2021-02-26 18:51:36 +0100

    ⚡️ Mensaje "Clase no encontrada"

---
+++ resources/views/modulos/errores/404/classroom.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', 'Classroom < Edunetwork') @section('content') <h4>La clase solicitada no se ha encontrado...</h4>
+@stop
+
+@section('css')
+    <style>
+
+    </style>
+@stop
+
+@section('js')
+    <script>
+    </script>
+@stop
+
+@section('footer')
+    Edunetwork v1.0
+</> by duoestudios
+@endsection
