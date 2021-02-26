12a8ab24e034928f0579d14a812e3bc902b21ae5
59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2021-02-26 15:29:56 +0100

    ⚡️ Creación de "Modal Classroom Wizard".

--- resources/views/modulos/classroom/index.blade.php
+++ resources/views/modulos/classroom/index.blade.php
@@ -1,6 +1,9 @@
 @extends('adminlte::page')
 
-@section('title', 'Classroom < Edunetwork') @section('content_header') <h1>Classroom</h1>
+@section('title', 'Classroom < Edunetwork') @section('content_header') <h1>Classroom @if (Auth::user()->hasRole('profesor')) <a class="btn btn-success btn-sm" data-toggle="modal"
+            data-target="#crearclase" href=""><i class="fas fa-chalkboard-teacher "></i> Crear clase</a></h1>
+    @endif
+    </h1>
 </h1> @stop
 
 @section('content')
@@ -33,7 +36,47 @@
             </div>
         </div>
     </div>
-
+    @if (Auth::user()->hasRole('profesor'))
+        <div class="modal fade" id="crearclase" tabindex="-1">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                <form class="modal-content" method="post" action="{{ url('/notificaciones/acciones/crear') }}">
+                    @csrf
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="crearclase">Crear nueva clase</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="asignatura">Asignatura</label>
+                            <input id="asignatura" name="asignatura" class="form-control form-control-sm" type="text"
+                                maxlength="255" value="" />
+                        </div>
+                        <div class="form-group">
+                            <label for="clase">Clase</label>
+                            <input id="clase" name="clase" class="form-control form-control-sm" type="text"
+                                maxlength="255" value="" required />
+                        </div>
+                        <div class="form-group">
+                            <label for="seccion">Sección</label>
+                            <input id="seccion" name="seccion" class="form-control form-control-sm" type="text"
+                                maxlength="255" value="" />
+                        </div>
+                        <div class="form-group">
+                            <label for="aula">Aula</label>
+                            <input id="aula" name="aula" class="form-control form-control-sm" type="text"
+                                maxlength="255" value="" />
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+                        <button type="submit" class="btn btn-primary">Añadir</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    @endif
 @stop
 
 @section('css')
@@ -74,10 +117,6 @@
 
 @section('js')
     <script>
-        $(document).ready(function() {
-            CKEDITOR.replace('contenido');
-        });
-
     </script>
 @stop
 
