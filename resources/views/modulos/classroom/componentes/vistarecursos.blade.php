eb202f8e777651a0d3ef9f677655b096bbe06a28
59341776 <59341776+alexandrumuntenas@users.noreply.github.com>
2021-03-05 23:11:00 +0100

    ⚡️ Los alumnos ven el dropdown de opciones. Corregido #4

--- resources/views/modulos/classroom/componentes/vistarecursos.blade.php
+++ resources/views/modulos/classroom/componentes/vistarecursos.blade.php
@@ -5,17 +5,20 @@
             <a href="{{ url('/elearning/c/' . $hash . '/trabajodeclase/v/' . $actividad->id) }}">
                 {{ $data['titulo'] }}
             </a>
-
-            <div class="card-tools dropwdown">
-                <button type="button" class="btn btn-tool" id="dropdownMenuButton" data-toggle="dropdown"
-                    aria-haspopup="true" aria-expanded="false"><i class="fas fa-ellipsis-v"></i>
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="{{ url('/elearning/c/' . $hash . '/trabajodeclase/e/' . $actividad->id) }}">Editar</a>
-                    <a class="dropdown-item" href="{{ url('/elearning/c/' . $hash . '/trabajodeclase/d/' . $actividad->id) }}">Eliminar</a>
-                    <button class="dropdown-item" disabled>Copiar enlace</a>
+            @if (Auth::user()->id === $classroom['classroom_teacher'])
+                <div class="card-tools dropwdown">
+                    <button type="button" class="btn btn-tool" id="dropdownMenuButton" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false"><i class="fas fa-ellipsis-v"></i>
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item"
+                            href="{{ url('/elearning/c/' . $hash . '/trabajodeclase/e/' . $actividad->id) }}">Editar</a>
+                        <a class="dropdown-item"
+                            href="{{ url('/elearning/c/' . $hash . '/trabajodeclase/d/' . $actividad->id) }}">Eliminar</a>
+                        <button class="dropdown-item" disabled>Copiar enlace</a>
+                    </div>
                 </div>
-            </div>
+            @endif
         </div>
         @break
         @case('')
